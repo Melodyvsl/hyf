b0c184a43835b59f2a44cc4cb0a401456790c2ab
Melodyvsl <42814809+Melodyvsl@users.noreply.github.com>
2018-10-30 07:20:14 -0700

    a

---
+++ HYF/week8(JS-4)/map_filter.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4];
+
+const odd = numbers.filter(i => i % 2 !== 0);
+
+const newNumbers = odd.map(i => {
+  if (odd===false) { 
+    return i; 
+  } else { 
+    return i * 2;}
+});
+
+
+console.log('The doubled numbers are', newNumbers); // ==> [2, 6]
